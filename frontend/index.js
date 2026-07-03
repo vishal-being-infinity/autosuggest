bcb639418d62974b19aacc56f03fa6181daa5113
vishal-being-infinity <vishal@being-infinity.com>
2026-07-03 19:24:37 +0530

    updated with toggle

--- frontend/index.js
+++ frontend/index.js
@@ -1,2 +1,23 @@
-console.log("Hello from JS");
-// alert("Hello from JS");
+const users = [
+  {
+    "name" :"Jane Doe",
+    "gender" : "Female",
+    "image" : "../images/jane.png"
+  },
+  {
+    "name" :"John Doe",
+    "gender" : "Male",
+    "image" : "../images/john.png"
+  },
+]
+let curUserId = 0;
+function toggle(){
+  if(curUserId == 0)
+    curUserId=1;
+  else
+    curUserId=0;
+  // image
+  document.getElementById("img").src=users[curUserId].image;
+  // name
+  document.getElementById("card-name").innerText=users[curUserId].name;
+}
