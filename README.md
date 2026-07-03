8790207471c359cce482c34aa3fbe99bae66f055
vishal-being-infinity <vishal@being-infinity.com>
2026-07-03 12:04:54 +0530

    web server set-up

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # autosuggest
 
 # this is my main repo for project !
+
+npm init -y
+
+npm install express
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+
+const PORT = 3002;
+
+// web server : 
+app.use(express.static("frontend"))
+
+app.listen(PORT, function(){
+  console.log("successfully running at http://localhost:"+PORT)
+})
+
+// 'npm install express' from terminal
